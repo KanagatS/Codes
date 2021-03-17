9af3ba512242796339822b5a53e34872832c3315
Kanagat <k_sapiya@kbtu.kz>
2021-03-17 15:46:43 +0600

    Codes

--- Real Midterm/test.py
+++ Real Midterm/test.py
@@ -1,23 +1,5 @@
-# f = open('test.txt')
-# for i in f:
-#     print(i)
-# f.close()
-
-# f = open('test2.txt', 'w')
-# f.write('Kanagat will recieve full on midterm\n')
-# f.write('Here is another second text\n')
-# f.write('Here is another third text\n')
-# f.close()
-
-# f = open('test2.txt', 'a')
-# f.write('The fourth text')
-
-# f = open('test2.txt', 'r')
-# print(f.read())
-
-# f = open('test3.txt', 'x')
-# f.write('Hello World')
-# f.close()
-
-import os
-os.remove('test.txt', 'test2.txt')
+n, m = map(int, input().split())
+l = list(map(int, input().split()))
+for i in range(n, m+1):
+    if i not in l:
+        print(i, end = ' ')
